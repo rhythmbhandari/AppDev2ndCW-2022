746d4fa9b8efdfc365fc52f7408a2515c8964122
amshuverma <pathosaion@gmail.com>
2022-05-05 18:16:25 +0545

    Created actor CRUD - Kinda

--- AppDev2ndCW-2022/Controllers/DVDController.cs
+++ AppDev2ndCW-2022/Controllers/DVDController.cs
@@ -1,12 +1,22 @@
+using AppDev2ndCW_2022.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppDev2ndCW_2022.Controllers;
 
 public class DVDController : Controller
 {
+    private readonly ILogger<DVDController> _logger;
+    public readonly DataBaseContext dataBaseContext;
+
+    public DVDController(ILogger<DVDController> logger, DataBaseContext db)
+    {
+        _logger = logger;
+        dataBaseContext = db;
+    }
     // GET
     public IActionResult AllMembers()
     {
+        ViewBag.message = "Success";
         return View();
     }
 
@@ -18,5 +28,18 @@ public class DVDController : Controller
     public IActionResult NotLoaned()
     {
         return View();
+    }
+
+    [Route("addActor")]
+    [AcceptVerbs("Get", "Post")]
+    public IActionResult AddActor(Actor actor)
+    {
+        if (Request.Method == "POST")
+        {
+            dataBaseContext.Add(actor);
+            dataBaseContext.SaveChanges();
+            return Redirect("/DVD/AllMembers");
+        }
+        return View("~/Views/Forms/AddActor.cshtml");
     }
 }
